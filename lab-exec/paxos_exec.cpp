efceef1f7931b0b9fdcc2801d7cd85410c7304a1
Madhura <madhuraparikh@gmail.com>
2014-11-01 23:57:31 -0500

    remove all LOGS to make more understandable

--- lab-exec/paxos_exec.cpp
+++ lab-exec/paxos_exec.cpp
@@ -16,7 +16,7 @@ void paxserver::execute_arg(const struct execute_arg& ex_arg)
         //LOG(l::DEBUG, "execute_arg msg for nid " << ex_arg.nid
         //    << "and rid " << ex_arg.rid
         //    << "received earlier (duplicate)\n");
-        LOG(l::DEBUG, "exec_arg duplicate: " << ex_arg << "\n");
+        //LOG(l::DEBUG, "exec_arg duplicate: " << ex_arg << "\n");
         return;
     }
     // create the viwestamp to send backups
@@ -32,16 +32,16 @@ void paxserver::execute_arg(const struct execute_arg& ex_arg)
         std::set<node_id_t> servers = get_other_servers(vc_state.view);
         for(const auto& serv : servers)
         {
-            LOG(l::DEBUG, id_str() << "pr replicate_arg msg now: " << net->now()
-                << "to server nid: " << serv << "\n");
+           // LOG(l::DEBUG, id_str() << "pr replicate_arg msg now: " << net->now()
+           //    << "to server nid: " << serv << "\n");
             send_msg(serv, std::make_unique<replicate_arg>(new_vs, ex_arg, latest_seen));
             
         }
     }
     else // otherwise send a not ok message to client with the id of the primary
     {
-        LOG(l::DEBUG, id_str() << "is not a primary now: " << net->now
-            << " send not ok to client rid: " << ex_arg.nid << "\n");
+        //LOG(l::DEBUG, id_str() << "is not a primary now: " << net->now
+        //    << " send not ok to client rid: " << ex_arg.nid << "\n");
         send_msg(ex_arg.nid, std::make_unique<execute_fail>(vc_state.view.vid, vc_state.view.primary, ex_arg.rid));
     }
     return;
@@ -51,8 +51,8 @@ void paxserver::execute_arg(const struct execute_arg& ex_arg)
 void paxserver::replicate_arg(const struct replicate_arg& repl_arg) {
     // log the current request if not already logged
     if (! paxlog.get_tup(repl_arg.arg.vs)) {
-        LOG(l::DEBUG, id_str() << " logging repl_arg msg from primary now: " << net->now
-            << "  " << repl_arg << "\n");
+        //LOG(l::DEBUG, id_str() << " logging repl_arg msg from primary now: " << net->now
+        //   << "  " << repl_arg << "\n");
         paxlog.log(repl_arg.arg.nid, repl_arg.arg.rid, repl_arg.vs, repl_arg.arg.request, get_serv_cnt(vc_state.view), net->now());
     }
     // execute all the requests <= the committed recvd from primary (DOUBT : should it be just less than (as in paper?))
@@ -60,7 +60,7 @@ void paxserver::replicate_arg(const struct replicate_arg& repl_arg) {
     {
         
         if (paxlog.next_to_exec(it) && (*it)->vs <= repl_arg.committed) {
-            LOG(l::DEBUG, id_str() << " executing repl_arg msg from primary now\n");
+            //LOG(l::DEBUG, id_str() << " executing repl_arg msg from primary now\n");
             // DOUBT : does the next_to_exec automatically update the latest_exec?
             paxop_on_paxobj(*it);
             paxlog.execute(*it); //DOUBT maybe this is handled by paxop_on_paxobj
@@ -70,8 +70,8 @@ void paxserver::replicate_arg(const struct replicate_arg& repl_arg) {
     // trim the log as possible - remove all the executed entries (they should be <= committed)
     paxlog.trim_front([](const std::unique_ptr<Paxlog::tup>&)->bool{return tup->executed;});
     // send repl_res ack to primary
-    LOG(l::DEBUG, id_str() << " sending repl_res msg to primary now "
-        << " for vs : "<< repl_arg.vs << "\n");
+    // LOG(l::DEBUG, id_str() << " sending repl_res msg to primary now "
+    //    << " for vs : "<< repl_arg.vs << "\n");
     send_msg(vc_state.view.primary, std::make_unique<replicate_res>(repl_arg.vs));
     return;
    //MASSERT(0, "replicate_arg not implemented\n");
@@ -84,8 +84,8 @@ void paxserver::replicate_res(const struct replicate_res& repl_res) {
         std::set<node_id_t> servers = get_other_servers(vc_state.view);
         for(const auto& serv : servers)
         {
-            LOG(l::DEBUG, id_str() << "pr accept_arg msg now: " << net->now()
-                << "to server nid: " << serv << "\n");
+            //LOG(l::DEBUG, id_str() << "pr accept_arg msg now: " << net->now()
+            //    << "to server nid: " << serv << "\n");
             send_msg(serv, std::make_unique<accept_arg>(latest_seen));
             
         }
@@ -97,13 +97,13 @@ void paxserver::replicate_res(const struct replicate_res& repl_res) {
     {
         
         if (paxlog.next_to_exec(it) && ((*it)->resp_cnt >= ((*it)->serv_cnt/2 + 1))) {
-            LOG(l::DEBUG, id_str() << " executing repl_res msg on primary.\n");
+            //LOG(l::DEBUG, id_str() << " executing repl_res msg on primary.\n");
             // DOUBT : does the next_to_exec automatically update the latest_exec?
             string result = paxop_on_paxobj(*it);
             paxlog.execute(*it); //DOUBT maybe this is handled by paxop_on_paxobj
             // send success message to client
-            LOG(l::DEBUG, id_str() << " sending success msg from primary now : " << net->now()
-                << " result: "<< result << "rid : " << (*it)->rid << "\n");
+            //LOG(l::DEBUG, id_str() << " sending success msg from primary now : " << net->now()
+            //    << " result: "<< result << "rid : " << (*it)->rid << "\n");
             send_msg((*it)->src, std::make_unique<execute_success>(result, (*it)->rid));
              // update the commited field to the latest executed viewstamp
             vc_state.latest_seen = (*it)->vs
@@ -122,7 +122,7 @@ void paxserver::accept_arg(const struct accept_arg& acc_arg) {
     for(auto it = paxlog.begin(); it != paxlog.end(); ++it)
     {
         if (paxlog.next_to_exec(it) && (*it)->vs <= acc_arg.committed) {
-            LOG(l::DEBUG, id_str() << " executing acc_arg msg from primary \n");
+            //LOG(l::DEBUG, id_str() << " executing acc_arg msg from primary \n");
             // DOUBT : does the next_to_exec automatically update the latest_exec?
             paxop_on_paxobj(*it);
             paxlog.execute(*it); //DOUBT maybe this is handled by paxop_on_paxobj
